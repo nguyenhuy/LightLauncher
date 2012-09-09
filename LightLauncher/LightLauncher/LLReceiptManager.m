8239a01877366cce80306920a6181d377cb4f8d1
Nguyen Thanh Huy <huy@cogini.com>
2012-09-09 17:48:59 +0700

    Test EmailCommand

--- LightLauncher/LightLauncher/LLReceiptManager.m
+++ LightLauncher/LightLauncher/LLReceiptManager.m
@@ -10,6 +10,13 @@
 #import "LLTwitterCommand.h"
 #import "LLEmailCommand.h"
 
+#import "LLToOption.h"
+#import "LLCcOption.h"
+#import "LLBccOption.h"
+#import "LLSubjectOption.h"
+#import "LLBodyOption.h"
+#import "LLAttachmentDataOption.h"
+
 @interface LLReceiptManager ()
 @property (nonatomic, strong, readwrite) NSMutableArray* receipts;
 @property (nonatomic, strong, readwrite) NSMutableArray* commands;
@@ -23,9 +30,33 @@
 - (id)init {
     self = [super init];
     if (self != nil) {
+        // Init fake email command to test
+        LLEmailCommand *emailCommand = [[LLEmailCommand alloc] initWithName:@"mail"];
+        emailCommand.toOptions = [NSMutableArray arrayWithObjects:
+                                     [[LLToOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                     [[LLToOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                     [[LLToOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                     nil];
+        emailCommand.ccOptions = [NSMutableArray arrayWithObjects:
+                                    [[LLCcOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                    [[LLCcOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                    [[LLCcOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                    nil];
+        emailCommand.bccOptions = [NSMutableArray arrayWithObjects:
+                                   [[LLBccOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                   [[LLBccOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                   [[LLBccOption alloc] initWithParam:@"allforone1511@gmail.com"],
+                                   nil];
+        emailCommand.subject = [[LLSubjectOption alloc] initWithParam:@"hello"];
+        emailCommand.body = [[LLBodyOption alloc] initWithParam:@"Hello LightLauncher" AndIsHtml:NO];
+        emailCommand.attachmentFiles = [NSMutableArray arrayWithObjects:
+                                        [[LLAttachmentDataOption alloc] initWithParam:@"d/acb.png" AndMimeType:@"image/png" andFileName:@"abc.png"],
+                                        [[LLAttachmentDataOption alloc] initWithParam:@"d/acb.png" AndMimeType:@"image/png" andFileName:@"abc.png"],
+                                        nil];
+        
         self.commands = [NSMutableArray arrayWithObjects:
-                         [[LLTwitterCommand alloc] initWithCommand:@"tw" AndOptions:nil],
-                         [[LLEmailCommand alloc] initWithCommand:@"em" AndOptions:nil],
+                         [[LLTwitterCommand alloc] initWithName:@"tw"],
+                         emailCommand,
                          nil];
     }
     return self;
