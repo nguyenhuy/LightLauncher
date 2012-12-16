c87989525442b9a5ac8981d7fb03ecd26c1e9910
Nguyen Thanh Huy <huy@cogini.com>
2012-12-16 16:29:14 +0700

    Add support for CoreData in AppDelegate

--- LightLauncher/LightLauncher/LLAppDelegate.h
+++ LightLauncher/LightLauncher/LLAppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @class LLCommandPrototypeTableViewController;
 
@@ -16,5 +17,13 @@
 
 @property (nonatomic, strong) LLCommandPrototypeTableViewController *viewController;
 @property (nonatomic, strong) UINavigationController *navigationController;
+
+// CoreData
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
