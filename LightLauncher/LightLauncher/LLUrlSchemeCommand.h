90b50077bba5c2e341b12a2ef96a54ecf9045203
Nguyen Thanh Huy <huy@cogini.com>
2013-01-21 22:53:58 +0700

    Add UrlSchemeCommand

---
+++ LightLauncher/LightLauncher/LLUrlSchemeCommand.h
@@ -0,0 +1,13 @@
+//
+//  LLUrlSchemeCommand.h
+//  LightLauncher
+//
+//  Created by Huy Nguyen on 1/21/13.
+//  Copyright (c) 2013 EarlyBird Lab. All rights reserved.
+//
+
+#import "LLCommand.h"
+
+@interface LLUrlSchemeCommand : LLCommand
+
+@end
---
+++ LightLauncher/LightLauncher/LLUrlSchemeCommand.m
@@ -0,0 +1,13 @@
+//
+//  LLUrlSchemeCommand.m
+//  LightLauncher
+//
+//  Created by Huy Nguyen on 1/21/13.
+//  Copyright (c) 2013 EarlyBird Lab. All rights reserved.
+//
+
+#import "LLUrlSchemeCommand.h"
+
+@implementation LLUrlSchemeCommand
+
+@end
